823f6d223831326bbfdf67450a0f09fbdf5b0ab3
srijithkaladharan <srijith1002@gmail.com>
2020-10-20 17:46:11 +0530

    Implemented asychronous capability to the application

--- src/main/java/com/srijith/redditclone/service/MailService.java
+++ src/main/java/com/srijith/redditclone/service/MailService.java
@@ -4,6 +4,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.srijith.redditclone.exceptions.SpringRedditException;
@@ -20,6 +21,8 @@ public class MailService {
 	private final JavaMailSender mailSender;
 	private final MailContentBuilder mailContentBuilder;
 
+	
+	@Async
 	void sendMail(NotificationEmail notificationEmail) {
 		MimeMessagePreparator messagePreparator = mimeMessage -> { 
 			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
